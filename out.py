c7181672d3468a98b200cc8d7b58c92b834ab1ec
Eugene Lin <me@eugenel.in>
2019-04-04 16:38:27 +0100

    Started intro to predicate logic co140

--- out.py
+++ out.py
@@ -4,4 +4,6 @@ output_path = os.path.realpath(".") + "/_output/"
 for d in filter(lambda x : os.path.isdir(x), os.listdir(".")):
     if d in [".git", "_output"]:
         continue
+    print("Copying " + d)
     shutil.copy(os.path.realpath(d) + "/main.pdf", output_path + d + ".pdf")
+print("Complete!")
