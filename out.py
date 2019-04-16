fa1a546d2d2f2e968075522e8f562819367605aa
Eugene Lin <me@eugenel.in>
2019-04-16 16:14:13 +0100

    Added page counter

--- out.py
+++ out.py
@@ -1,9 +1,15 @@
 import os
 import shutil
 output_path = os.path.realpath(".") + "/_output/"
+total_page = 0
 for d in filter(lambda x : os.path.isdir(x), os.listdir(".")):
     if d in [".git", "_output"]:
         continue
     print("Copying " + d)
     shutil.copy(os.path.realpath(d) + "/main.pdf", output_path + d + ".pdf")
+    cmd = "pdfinfo " + d + "/main.pdf | grep 'Pages' | awk '{print $2}'"
+    pages = int(os.popen(cmd).read().strip())
+    total_page += pages
+    print(d + ": " + str(pages) + " pages")
 print("Complete!")
+print("Total pages: " + str(total_page))
