1f540b85dc58efd7e270e003cdf0a7f3b28ea6f9
Eugene Lin <me@eugenel.in>
2019-04-29 18:58:00 +0100

    Updated page 16 of CO150

--- out.py
+++ out.py
@@ -3,7 +3,7 @@ import shutil
 output_path = os.path.realpath(".") + "/_output/"
 total_page = 0
 for d in filter(lambda x : os.path.isdir(x), os.listdir(".")):
-    if d in [".git", "_output"]:
+    if d in [".git", "_output", "programming"]:
         continue
     print("Copying " + d)
     shutil.copy(os.path.realpath(d) + "/main.pdf", output_path + d + ".pdf")
