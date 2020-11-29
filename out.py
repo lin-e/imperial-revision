eb91d862f8cbbe26cf0e0298075f5872eb511edb
me@eugenel.in <me@eugenel.in>
2020-11-29 22:16:21 +0000

    Rename function

--- out.py
+++ out.py
@@ -7,10 +7,10 @@ output_path = os.path.realpath(".") + "/_output/"
 total_pages = { }
 total_lines = { }
 
-def remap(y):
-    if y == '5':
+def get_year(mod):
+    if mod == "co572":
         return '3'
-    return y
+    return mod[2]
 
 def scan(p, pre):
     dirs = list(os.listdir(p))
@@ -20,13 +20,13 @@ def scan(p, pre):
         if not (d[0:2] == "co" and d[2].isdigit()) or d in exclude:
             continue
         print("Copying " + p + "/" + d)
-        year = "y" + remap(d[2]) + "/"
+        year = "y" + get_year(d) + "/"
         shutil.copy(os.path.realpath(p + "/" + d) + "/main.pdf", output_path + year + pre + d + ".pdf")
         cmd = "pdfinfo " + p + "/" + d + "/main.pdf | grep 'Pages' | awk '{print $2}'"
         pages = int(os.popen(cmd).read().strip())
         lines = sum(1 for line in open(os.path.realpath(p + "/" + d) + "/main.tex"))
-        total_pages[pre + "y" + remap(d[2])] = total_pages.get(pre + "y" + remap(d[2]), 0) + pages
-        total_lines[pre + "y" + remap(d[2])] = total_lines.get(pre + "y" + remap(d[2]), 0) + lines
+        total_pages[pre + "y" + get_year(d)] = total_pages.get(pre + "y" + get_year(d), 0) + pages
+        total_lines[pre + "y" + get_year(d)] = total_lines.get(pre + "y" + get_year(d), 0) + lines
         print(d + ": " + str(pages) + " pages")
         print(d + ": " + str(lines) + " lines")
 
